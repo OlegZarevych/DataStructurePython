09f4982992551197683a1da6c1b771a80920de15
Oleg Zarevych <Oleg.Zarevych@gmail.com>
2018-08-01 19:29:48 +0300

    Add Queue implementation

--- Queue.py
+++ Queue.py
@@ -2,4 +2,24 @@
 #Queue implementation
 
 class Queue:
-	#TODO
+	
+	def __init__(self):
+		self.items = []
+		self.count = 0
+		
+	def isEmpty(self):
+		return self.items == []
+	
+	def push(self, item):
+		self.items.append(item)
+		self.count += 1
+		
+	def pop(self):
+		if len(self.items) <= 0:
+			return ("Queue Empty!")
+		item = self.items.pop(0)
+		self.count -= 1
+		return item
+	
+	def size(self):
+		return self.count
