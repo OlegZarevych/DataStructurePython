e41592078f44218857480ba6479a6b7e1b01c787
Oleg Zarevych <Oleg.Zarevych@gmail.com>
2018-11-29 10:17:15 +0200

    implement max heap structure

---
+++ MaxHeap.py
@@ -0,0 +1,22 @@
+#! /usr/bin/python
+#Max heap implementation
+from itertools import count
+
+class max_heap:
+    
+    count = 0
+    
+    def push(self, data):
+        pass
+    
+    def pop(self):
+        pass
+    
+    def remove(self):
+        pass
+    
+    def length(self):
+        return count
+    
+    def is_empty(self):
+        return count == 0;
