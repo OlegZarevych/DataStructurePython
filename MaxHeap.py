5b702dc06895f3e0edc62aca2b902ab2cc949707
Oleg Zarevych <Oleg.Zarevych@gmail.com>
2018-12-01 03:16:56 +0200

    implement push and sorting logic (2 sortings)

--- MaxHeap.py
+++ MaxHeap.py
@@ -1,14 +1,45 @@
 #! /usr/bin/python
 #Max heap implementation
-from itertools import count
+
+import math
 
 class max_heap:
     
-    count = 0
+    def __init__(self):
+        self.heap = []
+        self.count = 0
+        
     
     def push(self, data):
-        pass
+        if self.count == 0:
+            self.heap.insert(self.count, data)
+            self.count += 1
+        
+        else:
+            self.count += 1
+            self.heap.insert(self.count, data)
+            self.__sort(len(self.heap) - 1)
+            #self.sort()
     
+    #another implementation
+    def sort(self):
+        current = self.count - 1
+        parent = math.floor(current / 2)     
+        while current > 0 and self.heap[parent] < self.heap[current]:
+            self.heap[current], self.heap[parent] = self.heap[parent], self.heap[current]
+            current = parent
+            parent = math.floor(current / 2)     
+        
+            
+    def __sort(self, current):
+        parent = math.floor(current / 2)     
+        if self.heap[parent] < self.heap[current]:
+            self.heap[current], self.heap[parent] = self.heap[parent], self.heap[current]
+            if parent <= 0:
+                return
+            else:
+                self.__sort(parent)
+                    
     def pop(self):
         pass
     
@@ -16,7 +47,10 @@ class max_heap:
         pass
     
     def length(self):
-        return count
+        return self.count
     
     def is_empty(self):
-        return count == 0;
+        return self.count == 0;
+    
+    def print(self):
+        print(self.heap)
