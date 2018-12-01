1ebed99c156a1ff8b02a038b8c1038181c099732
Oleg Zarevych <Oleg.Zarevych@gmail.com>
2018-12-01 03:17:22 +0200

    add max heap for using

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import LinkedList
 from BinaryTree import *
 from Stack import *
 from Queue import *
+from MaxHeap import max_heap
 
 def main():
     
@@ -12,7 +13,7 @@ def main():
     ll.add_node(2)
     ll.add_node(3)
     ll.list_print()
-    
+     
     print("Binary tree implementation")
     tree = BinaryTree(1)
     tree.insert(2)
@@ -21,7 +22,7 @@ def main():
     tree.insert(3)
     tree.print_tree()
     print(tree.search(8))
-
+ 
     print("Stack implementation")
     stack = Stack()
     print(stack.size)
@@ -33,13 +34,27 @@ def main():
     print(stack.pop())
     print(stack.pop())
     print(stack.size())
-    
+     
     print("Queue implementation")
     q = Queue()
     q.push("first")
     q.push("second")
     print(q.pop())
     print(q.pop())
+    
+    print("Heap implementation")
+    h = max_heap()
+    h.push(10)
+    h.push(3)
+    h.push(9)
+    h.push(7)
+    h.push(14)
+    h.push(8)
+    h.push(16)
+    h.push(2)
+    h.push(1)
+    h.push(4)
+    h.print()
 
 if __name__ == "__main__":
     main()
